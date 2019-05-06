0e35fde13347ca1cd25369a0a68f5e37216d2977
jasjenand <jasjenand@gmail.com>
2019-05-06 04:13:10 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 #import libraries & assign csv file
 import pandas as pd
 import numpy as np
+
 hot100 = pd.read_csv("HotStuff2.csv")
+pfork = pd.read_csv("newps4k.csv")
 
 #order csv file by week and chart position
 hot100["WeekID"]=pd.to_datetime(hot100["WeekID"])
 hot100.sort_values(["WeekID", "Week Position"], inplace=True, ascending=True)
 
 hot100.set_index("WeekID").head(101)
+pfork.head(30)
