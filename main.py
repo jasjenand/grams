4e572d2ba278398fef592e863aec7d14bec857dd
jasjenand <jasjenand@gmail.com>
2019-05-06 04:25:00 -0400

    All files imported
    Now includes hot 100 charts, Grammy winners, rolling stones 500 greatest albums, and 20,000 pitchfork reviews

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
-#import libraries & assign csv files
+#import libraries & assign csv/json files
 import pandas as pd
 import numpy as np
+import json
 
 hot100 = pd.read_csv("HotStuff2.csv")
 pfork = pd.read_csv("newps4k.csv")
 goat = pd.read_csv("goats.csv").set_index("Number")
+with open("grammyData.json") as file:
+    winners = json.load(file)
+#winners is Grammy winners in csv file
 
 #order hot100.csv file by week and chart position
 hot100["WeekID"]=pd.to_datetime(hot100["WeekID"])
