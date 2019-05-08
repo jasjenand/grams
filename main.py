84146e586585e23a3e120ba323691246a1efb1d2
jasjenand <jasjenand@gmail.com>
2019-05-08 03:55:42 -0400

    Update main.py

--- main.py
+++ main.py
@@ -27,7 +27,29 @@ def findGOATalbum(album):
         display(goat.loc[goat["Album"] == album])
     else:
         pass
+    
+def findOthers(year):
+    if goat["Year"].isin([year]).any():
+        goatNum = len(goat.loc[goat["Year"] == year])
+        if goatNum > 1:
+            print("Actually, " + str(goatNum) + " GOAT albums came out that year.")
+            display(goat.loc[goat["Year"] == year])
+    else:
+        pass
 
+def findGOATartist(artist):
+    if goat["Artist"].str.contains(artist).any():
+        albumCount = len(goat.loc[goat["Artist"] == artist])
+        print("They do have " + str(albumCount) + " GOAT album(s) though.")
+        display(goat.loc[goat["Artist"] == artist])
+        
+def findP4Krating(album):
+    if pfork["album"].str.contains(album).any():
+        rating = pfork.loc[pfork["album"] == album]
+        score = rating.iloc[0]["score"]
+        artistName = rating.iloc[0]["artist"]
+        print("Pitchfork rated %s by %s: %g out of 10." % (album, artistName, score))
+        
 #BILLBOARD HOT 100 SECTION
 #order hot100.csv file by week and chart position
 hot100["WeekID"]=pd.to_datetime(hot100["WeekID"])
@@ -39,33 +61,59 @@ hot100.set_index("WeekID")
 while True:
     interest = input("Are you interested in artists, albums, or songs? ")
     if interest.lower() not in ("artists", "albums", "songs"):
-        print("Try again sweetie.")
+        print("try again sweetie.")
     else:
         break
     
 if interest.lower() == "artists":
-    artist = input("Pick an artist: ")
-    display(winners.loc[winners["name"] == artist])
+    artistQuery = input("Pick an artist: ")
+    if winners["name"].str.contains(artistQuery).any():
+        gramCount = len(winners.loc[winners["name"] == artistQuery])
+        print("Total number of Grammy awards: " + str(gramCount))
+        display(winners.loc[winners["name"] == artistQuery])
+    else:
+        print("They don't have any Grammy Awards.")
+        findGOATartist(artistQuery)
+           
 elif interest.lower() == "albums":
-    album = input("Pick an album: ")
-    display(winners.loc[winners["awardFor"] == album])
-elif interest.lower() == "songs":
-    song = input("Pick a song: ")
-    display(winners.loc[winners["awardFor"] == song])
+    albumQuery = input("Pick an album: ")
+    findP4Krating(albumQuery) #DANGER WILL ROBINSON
+    if winners["awardFor"].str.contains(albumQuery).any():
+        gramCount = len(winners.loc[winners["awardFor"] == albumQuery])
+        print("Total number of Grammy awards: " + str(gramCount))
+        display(winners.loc[winners["awardFor"] == albumQuery])
+    else:
+        print("This album has no Grammy Awards.")
+        findGOATalbum(albumQuery)    
     
+elif interest.lower() == "songs":
+    songQuery = input("Pick a song: ")
+    if winners["awardFor"].str.contains(songQuery).any():
+        gramCount = len(winners.loc[winners["awardFor"] == songQuery])
+        print("Total number of Grammy awards: " + str(gramCount))
+        display(winners.loc[winners["awardFor"] == songQuery])
+    else:
+        print("This song has no Grammy Awards.")
+        
+        
 #don't pick a Best New Artist category
 index = int(input("Type the index number of a category you're interested in: "))
 winners.iloc[index]
 
 if winners.iloc[index].str.contains("Album").any():
     albumQuery = winners.iloc[index]["awardFor"]
+    albumYear = winners.iloc[index]["year"] - 1
     albumGrammyInfo(albumQuery)
+    findP4Krating(albumQuery)
     findGOATalbum(albumQuery)
+    findOthers(albumYear)
+    
 else:
     songQuery = winners.iloc[index]["awardFor"]
     songGrammyInfo(songQuery)
 
 #PITCHFORK REVIEWS SECTION
+pfork["date"] = pd.to_datetime(pfork["date"])
 pfork.loc[pfork["artist"] == "Chance the Rapper"] #can be any artist duh
 
 
@@ -92,3 +140,16 @@ def findGOATartist(artist):
 
 artistQuery = input("Does this artist have GOATs? ")
 findGOATartist(artistQuery)
+
+#search by year
+def findGOATyear(year):
+    if goat["Year"].isin([year]).any():
+        goatNum = len(goat.loc[goat["Year"] == year])
+        print(str(goatNum) + " GOAT albums came out that year.")
+        return goat.loc[goat["Year"] == year]
+    else:
+        print("That is all.")
+        print(type(goat["Year"]))
+
+yearQuery = int(input("Any GOATs this year? "))
+findGOATyear(yearQuery)
